99d9281e6b059edecf8897f52d2193b776eae4ea
Anton Goroshko <radjahair@gmail.com>
2020-11-17 13:10:06 +1000

    Change quotes container

--- js/carousel.js
+++ js/carousel.js
@@ -21,12 +21,6 @@ $(document).ready(function(){
     responsive: {
       0: {
         items: 1
-      },
-      768: {
-        items: 2
-      },
-      1200: {
-        items: 3
       }
     }
   });
